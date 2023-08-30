27a439360233085312c5e9f055d3bb0bc6200caa
Serinajefthas <serina.jefthas@gmail.com>
2023-08-30 06:07:42 -0700

    Fix else statement task 1 and 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -25,9 +25,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		new_node->right = parent->right;
 		parent->right->parent = new_node;
 	}
-	else
-		new_node->right = NULL;
-
 	parent->right = new_node;
 
 	return (new_node);
